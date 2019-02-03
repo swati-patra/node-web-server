e867bbaff5a1993f2070dc22a8f02b90e8858d04
swati patra <iswatipatra@gmail.com>
2019-02-03 12:51:12 +0530

    Add GET /todos/id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://localhost:27017/Users');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
  
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {user} = require('./models/user');
+var {User} = require('./models/user');
 
 
 var app = express();                   // app stores the application of express
@@ -30,6 +30,25 @@ app.get('/todos',(req,res) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/todos/:id',(req,res) => {
+    
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+    //res.send(req.params);
+});
+
 
 app.listen(3000, () => {                //listening to a port
 
