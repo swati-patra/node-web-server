e066334df013d8741db5f3ceedfe6fbb1a7629a7
swati patra <iswatipatra@gmail.com>
2019-02-06 18:27:18 +0530

    delete /todos/:id

---
+++ playgraound/mongoose-remove.js
@@ -0,0 +1,19 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+//Todo.remove({})  //removes everything;
+
+// Todo.remove({}).then((docs) => {
+//     console.log(docs);
+// })
+
+//Todo.findOneAndRemove   -we get information back
+//Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5c5ad5822b5540b2416153d8').then((results)=> {
+    console.log(results);
+})
--- server/server.js
+++ server/server.js
@@ -51,6 +51,23 @@ app.get('/todos/:id',(req,res) => {
 });
 
 
+app.delete('/todos/:id',(req,res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((doc)=> {
+        if(!doc){
+            return res.status(404).send();
+        }
+        res.status(200).send({doc});
+    }).catch((e)=> {
+        res.status(400).send();
+    })
+});
+
 app.listen(port, () => {                //listening to a port
 
     console.log(`started on port ${port}`);
